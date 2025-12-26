7c14ed5170104bc9f1df5845cf2e84e277a41d52
natnael0024 <nathanleg23@gmail.com>
2025-12-26 16:17:35 +0300

    commit

--- components/ScopeCard.tsx
+++ components/ScopeCard.tsx
@@ -3,7 +3,6 @@
 import { Card, CardHeader, CardTitle, CardContent } from "@/components/ui/card";
 import ReactMarkdown from "react-markdown";
 import remarkGfm from "remark-gfm";
-import { GlowingEffect } from "@/components/ui/glowing-effect";
 
 interface ScopeCardProps {
   title: string;
@@ -14,18 +13,6 @@ interface ScopeCardProps {
 export default function ScopeCard({ title, content, children }: ScopeCardProps) {
   return (
     <Card className="mb-4 relative overflow-hidden rounded-2xl border-gray-200 dark:border-gray-700">
-      {/* Make sure the glow container covers the card */}
-      <div className="absolute inset-0">
-        <GlowingEffect
-          spread={40}
-          glow={true}
-          disabled={false}
-          proximity={64}
-          inactiveZone={0.01}
-        />
-      </div>
-
-      {/* Content must be above glow */}
       <div className="relative z-10">
         <CardHeader>
           <CardTitle>{title}</CardTitle>
